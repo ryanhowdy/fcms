7f9b41c8e90a7ed4db1a5f8e1e4456bbb21e7574
ryanhowdy <r.haudenschilt@gmail.com>
2013-03-17 15:43:45 -0500

    Cleaned up some old tests.

--- test/inc-util-get_theme.php
+++ test/inc-util-get_theme.php
@@ -4,7 +4,11 @@ require_once dirname(dirname(__FILE__)).'/test/lib/utils.php';
 
 require_once TEST.'lib/Test-More.php';
 require_once INC.'config_inc.php';
+require_once INC.'User.php';
+require_once INC.'Error.php';
+require_once INC.'Database.php';
 require_once INC.'utils.php';
+require_once INC.'constants.php';
 
 diag("getTheme");
 
@@ -15,5 +19,5 @@ connectDatabase();
 $theme_no_id   = getTheme();
 $theme_bad_id  = getTheme(0);
 
-is($theme_no_id, ROOT.'themes/default/', 'no userid');
-is($theme_bad_id, ROOT.'themes/default/', 'bad userid');
+is($theme_no_id, UI.'themes/default/', 'no userid');
+is($theme_bad_id, UI.'themes/default/', 'bad userid');
