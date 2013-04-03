e4802ffa40eb4064d3b5c9a44f9d31fe754c120b
ryanhowdy <r.haudenschilt@gmail.com>
2013-04-03 09:07:00 -0500

    Updated support link in footer to point to new support.familycms.com

--- familyconnections/ui/themes/default/footer.php
+++ familyconnections/ui/themes/default/footer.php
@@ -5,7 +5,7 @@
     <div id="footer">
         <p>
             <a href="<?php echo $TMPL['path']; ?>index.php" class="ft"><?php echo T_pgettext('The beginning or starting place.', 'Home'); ?></a> | 
-            <a href="http://www.familycms.com/forum/index.php" class="ft"><?php echo T_('Support Forum'); ?></a> | 
+            <a href="http://support.familycms.com/" class="ft"><?php echo T_('Support'); ?></a> | 
             <a href="<?php echo $TMPL['path']; ?>help.php" class="ft"><?php echo T_('Help'); ?></a><br />
             <a href="http://www.familycms.com"><?php echo $TMPL['version']; ?></a> - Copyright &copy; 2006-<?php echo $TMPL['year']; ?> Ryan Haudenschilt.
         </p>
