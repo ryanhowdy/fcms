9b4a5d107ea5aeea6ef9585838e967219fbe606a
ryanhowdy <r.haudenschilt@gmail.com>
2013-06-03 09:48:20 -0500

    fixed bug with month names.

--- familyconnections/inc/datetime.php
+++ familyconnections/inc/datetime.php
@@ -367,11 +367,11 @@ function formatBirthday ($year, $month, $day)
         {
             if (!empty($day))
             {
-                return "$year-$month-$day";
+                return getMonthName((int)$month).', '.$day.' '.$year;
             }
             else
             {
-                return getMonthName($month).', '.$year;
+                return getMonthName((int)$month).', '.$year;
             }
         }
         else
@@ -383,7 +383,7 @@ function formatBirthday ($year, $month, $day)
     {
         if (!empty($day))
         {
-            return getMonthAbbr($month).'. '.$day;
+            return getMonthAbbr((int)$month).'. '.$day;
         }
     }
 
