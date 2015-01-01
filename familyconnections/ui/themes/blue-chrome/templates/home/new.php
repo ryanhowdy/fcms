7c2eb40e9ef61a37e7d520b697831425540729c7
ryanhowdy <r.haudenschilt@gmail.com>
2015-01-01 09:19:48 -0600

    Updated blue-chrome theme to work with 3.5.0.

---
+++ familyconnections/ui/themes/blue-chrome/templates/home/new.php
@@ -0,0 +1,55 @@
+        <h2><?php echo $TMPL['textWhatsNew']; ?></h2>
+
+<?php foreach ($TMPL['new'] as $new): ?>
+    <?php if (isset($new['textDateHeading'])): ?>
+        <p><b><?php echo $new['textDateHeading']; ?></b></p>
+    <?php else: ?>
+        <div id="<?php echo $new['position']; ?>" class="new <?php echo $new['class']; ?>">
+            <div class="avatar">
+                <img src="<?php echo $new['avatar']; ?>" alt="<?php echo $new['displayname']; ?>"/>
+            </div>
+            <div class="info">
+                <a class="u" href="profile.php?member=<?php echo $new['userId']; ?>"><?php echo $new['displayname']; ?></a> &nbsp;- &nbsp;
+                <small><i><?php echo $new['timeSince']; ?></i></small>
+                <p><?php echo $new['textInfo']; ?></p>
+
+        <?php if (isset($new['title']) && !empty($new['title'])): ?>
+                <div class="object">
+                    <h5><?php echo $new['title']; ?></h5>
+                    <?php echo $new['details']; ?>
+                </div>
+        <?php endif; ?>
+
+        <?php if (isset($new['children'])): ?>
+            <?php foreach ($new['children'] as $child): ?>
+                <div class="child <?php echo $child['class']; ?>">
+                    <div class="avatar">
+                        <img src="<?php echo $child['avatar']; ?>" alt="<?php echo $child['displayname']; ?>"/>
+                    </div>
+                    <div class="info">
+                        <a class="u" href="profile.php?member=<?php echo $child['userId']; ?>"><?php echo $child['displayname']; ?></a> &nbsp;- &nbsp;
+                        <small><i><?php echo $child['timeSince']; ?></i></small>
+                        <p><?php echo $child['textInfo']; ?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+
+        <?php if (isset($new['textReply'])): ?>
+                <div id="status_reply">
+                    <form method="post" action="home.php">
+                        <textarea id="status" name="status" placeholder="<?php echo $new['textReply']; ?>" title="<?php echo $new['textReply']; ?>"></textarea>
+                        <input type="hidden" id="parent" name="parent" value="<?php echo $new['replyParentId']; ?>"/>
+                        <input type="submit" id="status_submit" name="status_submit" value="<?php echo $new['textReply']; ?>"/>
+                    </form>
+                </div>
+        <?php endif; ?>
+
+            </div>
+        </div>
+    <?php endif; ?>
+<?php endforeach; ?>
+
+        <p class="alignright">
+            <a class="rss" href="rss.php?feed=all"><?php echo $TMPL['textRssFeed']; ?></a>
+        </p>
---
+++ familyconnections/ui/themes/blue-chrome/templates/poll/polls.php
@@ -0,0 +1,19 @@
+            <h2><?php echo $TMPL['textPastPolls']; ?></h2>
+            <table class="sortable">
+                <thead>
+                    <tr>
+                        <th><?php echo $TMPL['textQuestion']; ?></th>
+                        <th><?php echo $TMPL['textDate']; ?></th>
+                        <th><?php echo $TMPL['textVotes']; ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($TMPL['polls'] as $poll): ?>
+                    <tr>
+                        <td><a href="<?php echo $poll['url']; ?>"><?php echo $poll['question']; ?></a></td>
+                        <td><?php echo $poll['date']; ?></td>
+                        <td><?php echo $poll['vote']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
