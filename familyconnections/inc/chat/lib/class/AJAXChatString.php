bb5ec69e327644a0d2659b6d1625e72e2a52f830
Francis Brito <fr.br94@gmail.com>
2015-04-30 14:55:25 -0400

    Fix static methods not defined as static.

--- familyconnections/inc/chat/lib/class/AJAXChatString.php
+++ familyconnections/inc/chat/lib/class/AJAXChatString.php
@@ -10,7 +10,7 @@
 // Class to provide multibyte enabled string methods
 class AJAXChatString {
 
-	function subString($str, $start=0, $length=null, $encoding='UTF-8') {
+	public static function subString($str, $start=0, $length=null, $encoding='UTF-8') {
 		if($length === null) {
 			$length = AJAXChatString::stringLength($str);
 		}		
@@ -21,9 +21,11 @@ class AJAXChatString {
 		} else {
 			return substr($str, $start, $length);
 		}
+
+                return str;
 	}
 	
-	function stringLength($str, $encoding='UTF-8') {
+	public static function stringLength($str, $encoding='UTF-8') {
 		if(function_exists('mb_strlen')) {
 			return mb_strlen($str, $encoding);
 		} else if(function_exists('iconv_strlen')) {
