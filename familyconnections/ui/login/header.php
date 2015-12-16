069cecb1a1e3c1afaec7e239e475f3db4bc5f8b0
ryanhowdy <r.haudenschilt@gmail.com>
2015-12-16 07:21:24 -0600

    Issue #409 - Move html to templates The login/index page

---
+++ familyconnections/ui/login/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo T_pgettext('Language Code for this translation', 'lang'); ?>" lang="<?php echo T_pgettext('Language Code for this translation', 'lang'); ?>">
+<head>
+<title><?php echo $TMPL['sitename']; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="author" content="Ryan Haudenschilt"/>
+<meta name="viewport" content="width=device-width" />
+<link rel="shortcut icon" href="ui/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="ui/css/fcms-core.css?version370"/>
+</head>
+<body <?php if (isset($TMPL['body'])) { echo $TMPL['body']; } ?>>
