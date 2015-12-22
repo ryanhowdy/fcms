b7e5d26c7f23cc98c5d50807bf2fa6adfa7ac406
ryanhowdy <r.haudenschilt@gmail.com>
2015-12-22 08:06:31 -0600

    Issue #409 - Move html to templates Documents page.

---
+++ familyconnections/ui/themes/default/templates/document/main.php
@@ -0,0 +1,44 @@
+
+    <?php if (isset($TMPL['pageNavigation'])): ?>
+<h1>page nav</h1>
+        <?php require_once(TEMPLATES.'global/page-navigation.php'); ?>
+    <?php endif; ?>
+
+    <?php if (isset($TMPL['documents'])): ?>
+
+            <script type="text/javascript" src="ui/js/tablesorter/js/jquery.tablesorter.js"></script>
+            <table id="docs" class="sortable">
+                <thead>
+                    <tr>
+                        <th class="sortfirstasc"><?php echo $TMPL['documentText']; ?></th>
+                        <th><?php echo $TMPL['descriptionText']; ?></th>
+                        <th><?php echo $TMPL['uploadedByText']; ?></th>
+                        <th><?php echo $TMPL['dateAddedText']; ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+        <?php foreach ($TMPL['documents'] as $d): ?>
+                    <tr>
+                        <td>
+                            <a href="?download=<?php echo $d['name']; ?>"><?php echo $d['name']; ?></a>
+                            <?php if (isset($d['delete'])): ?><?php echo $d['delete']; ?><?php endif; ?>
+                        </td>
+                        <td><?php echo $d['description']; ?></td>
+                        <td><?php echo $d['user']; ?></td>
+                        <td><?php echo $d['date']; ?></td>
+                    </tr>
+        <?php endforeach; ?>
+
+                </tbody>
+            </table>
+
+    <?php else: ?>
+
+            <div class="blank-state">
+                <h2><?php echo $TMPL['blankStateHeaderText']; ?></h2>
+                <h3><?php echo $TMPL['blankStateText']; ?></h3>
+                <h3><a href="?adddoc=yes"><?php echo $TMPL['blankStateLinkText']; ?></a></h3>
+            </div>
+
+    <?php endif; ?>
--- familyconnections/ui/themes/default/templates/global/page-navigation.php
+++ familyconnections/ui/themes/default/templates/global/page-navigation.php
@@ -1,18 +1,27 @@
-        <?php if (isset($TMPL['navigation'])): ?>
+    <?php if (isset($TMPL['pageNavigation'])): ?>
+
+        <?php if (isset($TMPL['pageNavigation']['section'])): ?>
+
             <div id="sections_menu">
                 <ul>
-            <?php foreach ($TMPL['navigation'] as $nav): ?>
-                    <li><a href="<?php echo $nav['url']; ?>"><?php echo $nav['textLink']; ?></a></li>
+            <?php foreach ($TMPL['pageNavigation']['section'] as $nav): ?>
+                    <li><a href="<?php echo $nav['url']; ?>"><?php echo $nav['text']; ?></a></li>
             <?php endforeach; ?>
                 </ul>
             </div>
+
         <?php endif; ?>
-        <?php if (isset($TMPL['actions'])): ?>
+
+        <?php if (isset($TMPL['pageNavigation']['action'])): ?>
+
             <div id="actions_menu">
                 <ul>
-            <?php foreach ($TMPL['actions'] as $action): ?>
-                    <li><a href="<?php echo $action['url']; ?>"><?php echo $action['textLink']; ?></a></li>
+            <?php foreach ($TMPL['pageNavigation']['action'] as $action): ?>
+                    <li><a href="<?php echo $action['url']; ?>"><?php echo $action['text']; ?></a></li>
             <?php endforeach; ?>
                 </ul>
             </div>
+
         <?php endif; ?>
+
+    <?php endif; ?>
