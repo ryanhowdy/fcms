350e36446bde89c75d721691d7bffbb629f50350
ryanhowdy <r.haudenschilt@gmail.com>
2015-12-29 19:48:53 -0600

    Issue #409 - Move html to templates Address book

---
+++ familyconnections/ui/themes/default/templates/addressbook/address.php
@@ -0,0 +1,55 @@
+
+            <div id="leftcolumn">
+                <?php require_once(TEMPLATES.'addressbook/menu.php'); ?>
+            </div>
+
+            <div id="maincolumn">
+
+                <div id="address-options">
+                    <ul>
+                        <?php foreach ($TMPL['addressOptions'] as $o): ?>
+                        <li id="<?php echo $o['liId']; ?>">
+                            <a id="<?php echo $o['aId']; ?>" href="<?php echo $o['url']; ?>"><?php echo $o['text']; ?></a>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+
+                <div id="address-details">
+                    <p>
+                        <img alt="avatar" src="<?php echo $TMPL['avatar']; ?>"/>
+                        <b class="name"><?php echo $TMPL['name']; ?></b>
+                    </p>
+                    <p>
+                        <b class="label"><?php echo $TMPL['addressText']; ?>:</b>
+                        <span class="data">
+                            <?php echo $TMPL['address']; ?>
+                            <?php if (isset($TMPL['mapText'])): ?>
+                            <a href="http://maps.google.com/maps?q=<?php echo $TMPL['addressUrl']; ?>"><?php echo $TMPL['mapText']; ?></a>
+                            <?php endif; ?>
+                        </span>
+                    </p>
+                    <p>
+                        <b class="label"><?php echo $TMPL['emailText']; ?>:</b>
+                        <span class="data">
+                            <?php echo $TMPL['email']; ?>
+                            <a class="email" href="mailto:<?php echo $TMPL['email']; ?>"
+                                title="<?php echo $TMPL['emailMemberText']; ?>">&nbsp;</a>
+                        </span>
+                    </p>
+                    <p>
+                        <b class="label"><?php echo $TMPL['homeText']; ?>:</b>
+                        <span class="data"><?php echo $TMPL['home']; ?></span>
+                    </p>
+                    <p>
+                        <b class="label"><?php echo $TMPL['workText']; ?>:</b>
+                        <span class="data"><?php echo $TMPL['work']; ?></span>
+                    </p>
+                    <p>
+                        <b class="label"><?php echo $TMPL['mobileText']; ?>:</b>
+                        <span class="data"><?php echo $TMPL['mobile']; ?></span>
+                    </p>
+                </div>
+
+            </div>
+
