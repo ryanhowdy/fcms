8fa915e10cf95628a9db9657e437b176ebc59db6
ryanhowdy <r.haudenschilt@gmail.com>
2016-03-04 09:39:31 -0600

    Issue #409 - Move html to templates Family News

---
+++ familyconnections/ui/themes/default/templates/familynews/news.php
@@ -0,0 +1,34 @@
+
+    <?php if (isset($TMPL['news']) && count($TMPL['news']) > 0): ?>
+        <?php foreach ($TMPL['news'] as $n): ?>
+            <div class="news-post">
+                <h2>
+                    <a href="<?php echo $n['url']; ?>"><?php echo $n['title']; ?></a>
+                </h2>
+                <span class="date">
+                    <?php echo $n['updated']; ?> - <?php echo $n['displayname']; ?>
+                </span>
+                <p>
+                    <?php if (isset($n['external'])): ?>
+                        <span style="background-color:#eee; color:#999; font-size:13px;">
+                            <?php echo $n['external']; ?>
+                        </span><br/>
+                    <?php endif; ?>
+                    <?php echo $n['news']; ?>
+                </p>
+                <p class="news-comments">
+                    <a href="<?php echo $n['url']; ?>#comments"><?php echo $n['commentsText']; ?></a> - <?php echo $n['commentCount']; ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+            <div class="blank-state">
+                <h2><?php echo $TMPL['nothingToSeeHereText']; ?></h2>
+                <h3><?php echo $TMPL['noOneAddedNewsText']; ?></h3>
+                <h3><?php echo $TMPL['beFirstAddNewsText']; ?></h3>
+                <ol>
+                    <li><a href="?addnews=yes"><?php echo $TMPL['addFamilyNewsText']; ?></a></li>
+                    <li><a href="settings.php?view=familynews"><?php echo $TMPL['importExistinBlogText']; ?></a></li>
+                </ol>
+            </div>
+    <?php endif; ?>
