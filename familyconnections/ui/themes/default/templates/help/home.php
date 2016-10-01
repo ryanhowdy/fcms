c99387d2ce7922ad7de8dc450650e7397c317daf
ryanhowdy <r.haudenschilt@gmail.com>
2016-10-01 18:09:10 +0000

    Issue #409 - Move html to templates Help

---
+++ familyconnections/ui/themes/default/templates/help/home.php
@@ -0,0 +1,8 @@
+            <div id="maincolumn">
+                <h2><?php echo T_('Welcome to the Help section.'); ?></h2>
+                <p><?php echo T_('Browse the topics to the left to find help on the most frequently asked topics.'); ?></p>
+                <p>&nbsp;</p>
+                <h3><?php echo T_('Need more help?'); ?></h3>
+                <p><?php echo T_('Check out the support forum for more help.'); ?></p>
+                <p><a href="http://familycms.tenderapp.com/discussions"><?php echo T_('Support Forum'); ?></a></p>
+            </div>
---
+++ familyconnections/ui/themes/default/templates/help/video-gallery.php
@@ -0,0 +1,14 @@
+
+        <div id="maincolumn">
+            <h4><?php echo T_('Video Gallery'); ?></h4>
+            <p><a href="#video-youtube-private"><?php echo T_('YouTube Private Videos.'); ?></a></p>
+            <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><hr/><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+
+            <p><a name="video-youtube-private">&nbsp;</a></p>
+            <p>&nbsp;</p>
+            <p><b><?php echo T_('YouTube Private Videos'); ?></b></p>
+            <p><?php echo T_('When you connect your Family Connections account with YouTube, a unique token is created which grants your Family Connections account access to view data on YouTube, just like if you were logged into YouTube.'); ?></p>
+            <p><?php echo T_('In order to keep your videos private on YouTube, but public to the members of your family site, Family Connections will use your unique token to let other members view your private videos.'); ?></p>
+            <p>&nbsp;</p>
+            <div class="top"><a href="#top"><?php echo T_('Back to Top'); ?></a></div>';
+        </div>
