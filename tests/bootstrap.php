0829b75f78c8f159a1a5e33de2e666754f107c70
ryanhowdy <r.haudenschilt@gmail.com>
2018-07-02 14:36:22 -0400

    Fix travisci build failures.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,3 +15,10 @@ require_once INC.'constants.php';
 require_once INC.'Error.php';
 require_once INC.'User.php';
 require_once INC.'Database.php';
+
+// Some travis environments use phpunit > 6
+$newClass = '\PHPUnit\Framework\TestCase';
+$oldClass = '\PHPUnit_Framework_TestCase';
+if (!class_exists($newClass) && class_exists($oldClass)) {
+    class_alias($oldClass, $newClass);
+}
