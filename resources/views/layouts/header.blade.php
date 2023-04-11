9053b20145cbb2e464ebd23a444650b738e78ea5
ryanhowdy <r.haudenschilt@gmail.com>
2023-04-11 14:22:20 +0000

    Added missing favicons.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,8 +7,8 @@
 <title>{{ config('app.name') }}</title>
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <script src="{{ mix('js/app.js') }}"></script>
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
 <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 </head>
